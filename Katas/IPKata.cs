4d2361e94099d1bd81fc0a86356e32d29b12a422
CHILLI\Abraham <abraham.neethling@chillisoft.co.za>
2018-08-17 10:55:59 +0200

    Test

---
+++ Katas/IPKata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class IPKata
+    {
+        public bool ValidateIpv4Address(string ip)
+        {
+            var iPparts = GetIpParts(ip);
+            return IsIpLessThan4Characters(iPparts) && (!ip.EndsWith("0") && !ip.EndsWith("255"));
+        }
+
+        private static bool IsIpLessThan4Characters(IReadOnlyCollection<string> iPparts)
+        {
+            return iPparts.Count >= 4;
+        }
+
+        private static string[] GetIpParts(string ip)
+        {
+            return ip.Split('.');
+        }
+    }
+}
